85a3731baab92d737e71697e3ad2fc5419b67d0f
ToldenLabs <nathenskaggs720@gmail.com>
2026-09-10 21:26:26 -0400

    Delete test directory

--- test/es6module/module-3250-bug-dep2.js
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------------------------------------------------------------------
-// Copyright (C) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
-//-------------------------------------------------------------------------------------------------------
-
-// module-3250-bug-dep2.js
-export * from './module-3250-bug-dep.js';
